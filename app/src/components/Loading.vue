0fca27af79a635d9510fe3af776498818ad60739
MZSkjaerstad <martskjae@hotmail.no>
2022-05-23 11:50:37 +0200

    :ambulance: Applying hotfixes to lottie animation

--- app/src/components/Loading.vue
+++ app/src/components/Loading.vue
@@ -1,14 +1,14 @@
 <template>
   <main class="loading">
-    <Vue3Lottie :animationData="loadingAnimation" :loop="true" :autoplay="true" :height="200" :width="200" />
+    <Vue3Lottie :animationData="loadingJSON" :loop="true" :autoplay="true" :height="50" :width="50" />
 
     <h2>loading</h2>
   </main>
 </template>
 
 <script>
-  import Vue3Lottie from 'vue3-lottie'
-  import loadingAnimation from '../lotties/loading.json'
+  import { Vue3Lottie } from 'vue3-lottie'
+  import loadingJSON from '../lotties/loading.json'
 
   export default {
     components: {
@@ -17,7 +17,7 @@
 
     data() {
       return {
-        loadingAnimation
+        loadingJSON
       }
     }
   }
@@ -28,6 +28,7 @@
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 }
