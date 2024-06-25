3d71669439745c02ba9d998492f79ed9627fe8ae
Martin Skjærstad <martinskjaerstad@Martins-MacBook-Pro-3.local>
2024-06-25 15:22:41 +0200

    :ambulance: Mobile video section hotfix

--- app/src/components/mal/exhibitionSections/VideoFsSection.vue
+++ app/src/components/mal/exhibitionSections/VideoFsSection.vue
@@ -20,7 +20,7 @@ export default {
 
   @media screen and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
     .video-fs-section {
-        padding: 0;
+        padding: 0 5%;
         height: 45vh;
     }
   }
