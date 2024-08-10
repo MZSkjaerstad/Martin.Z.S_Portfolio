4b81333dc3219a4876e7125610874c2e860d964a
Martin Skjærstad <martinskjaerstad@Martins-MBP-3.home>
2024-08-10 20:27:16 +0200

    :ambulance: fix video player height on mobile

--- app/src/components/mal/exhibitionSections/VideoFsSection.vue
+++ app/src/components/mal/exhibitionSections/VideoFsSection.vue
@@ -1,5 +1,7 @@
 <template>
-   <iframe class="video-fs-section" :src="sectionData.videoUrl" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+   <div class="video-fs-section">
+      <iframe :src="sectionData.videoUrl" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+   </div>
 </template>
 
 <script>
@@ -12,16 +14,26 @@ export default {
 
 <style>
   .video-fs-section {
+    position: relative;
     width: 100%;
-    height: 90vh;
-    padding: 0 7%;
+    height: 0;
+    padding: 0 0 56.25% 0;
     margin: var(--spacing-section) 0 0 0;
+    overflow: hidden;
+  }
+
+  .video-fs-section iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    padding: 0 7%;
   }
 
   @media screen and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
-    .video-fs-section {
-        padding: 0 5%;
-        height: 45vh;
+    .video-fs-section iframe {
+      padding: 0 5%;
     }
   }
 
